2feb64315340c03682c342b62153db94c8822fe8
Daniella Marin <kdmonteal@gmail.com>
2026-03-05 12:03:45 -0500

    EDIFICIOS

--- portfolio/class5/src/js/main.js
+++ portfolio/class5/src/js/main.js
@@ -13,13 +13,14 @@ function start() {
 
 function initScene() {
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xe0e0d1 );
+    scene.background = new THREE.Color( 0xD7F0F7 );
+    scene.fog = new THREE.FogExp2( 0x9db3b5, 0.002 );
 
     camera = new THREE.PerspectiveCamera( 
         75,                                     // Ángulo "grabación" - De abaja -> Arriba 
         window.innerWidth / window.innerHeight, // Relación de aspecto 16:9
-        0.1,                                    // Mas cerca (no renderiza) 
-        1000                                    // Mas lejos (no renderiza)
+        1,                                    // Mas cerca (no renderiza) 
+        10000                                    // Mas lejos (no renderiza)
     );
 
     // renderer = new THREE.WebGLRenderer();
@@ -40,6 +41,39 @@ function initScene() {
 
     var axesHelper = new THREE.AxesHelper( 1 );
     scene.add( axesHelper );
+
+
+
+    //////
+    var geo = new THREE.BoxGeometry( 1, 1, 1 );     
+    geo.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
+    var mat = new THREE.MeshPhongMaterial( { color: 0xcccccc, flatShading: true } );
+
+    for ( var i = 0; i < 300; i ++ ) {
+        var building = new THREE.Mesh( geo.clone(), mat.clone() );
+        building.position.x = Math.floor( Math.random() * 200 - 100 ) * 8;
+        building.position.z = Math.floor( Math.random() * 200 - 100 ) * 8;
+        building.scale.x = Math.random() * 50 + 10;
+        building.scale.y = Math.random() * building.scale.x * 8 + 8;
+        building.scale.z = building.scale.x;
+        scene.add( building );
+    }
+
+    camera.position.y = 400;
+    camera.position.z = 400;
+    camera.rotation.x = -45 * Math.PI / 180;
+
+
+    var ground = new THREE.PlaneGeometry( 2000, 2000, 20, 20 );
+    var groundMaterial = new THREE.MeshBasicMaterial( { color: 0x9db3b5, overdraw:true } );
+    var groundMesh = new THREE.Mesh( ground, groundMaterial );
+    groundMesh.rotation.x = -90 * Math.PI / 180;
+    scene.add( groundMesh );
+
+    var light = new THREE.DirectionalLight( 0xf6e86e, 1 );
+    light.position.set( 1, 3, 2 );
+    scene.add( light ); 
+
 }
 
 function animate() {
@@ -72,6 +106,8 @@ function getBuildingData() {
 }
 
 function drawElement() {
+
+    // box, plane
     // aqui va toda la logica de dibujar el edificio, usando los datos que se ingresaron en el prompt
     alert("creo el edificio con los datos ingresados");
 }
@@ -83,3 +119,6 @@ function cleanParamsUI(datos, marker) {
     }
     return value;
 }
+
+
+
